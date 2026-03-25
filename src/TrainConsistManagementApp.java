7714073e729136e8b50d26d0f4a964da17569714
Amith-P-J <amithpj05@gmail.com>
2026-03-25 09:45:47 +0530

    UC2: Track Unique Bogie IDs (Set – HashSet)

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -5,31 +5,29 @@ public class TrainConsistManagementApp {
     public static void main(String[] args) {
 
         System.out.println("==== Train Consist Management App ====");
-        System.out.println("==== UC2 - Add Passenger Bogies to Train ====\n");
-
-        List<String> passengerBogies = new ArrayList<>();
-
-
-        passengerBogies.add("Sleeper");
-        passengerBogies.add("AC Chair");
-        passengerBogies.add("First Class");
+        System.out.println("==============================================\n");
+        System.out.println("==== UC3 - Track Unique Bogie IDs ====\n");
+        System.out.println("==============================================\n");
 
 
-        System.out.println("After Adding Bogies:");
-        System.out.println("Passenger Bogies : " + passengerBogies);
+        Set<String> bogies = new HashSet<>();
+
+        bogies.add("BG101");
+        bogies.add("BG102");
+        bogies.add("BG103");
+        bogies.add("BG104");
 
 
-        passengerBogies.remove("AC Chair");
-        System.out.println("\nAfter Removing 'AC Chair':");
-        System.out.println("Passenger Bogies : " + passengerBogies);
+        bogies.add("BG101");
+        bogies.add("BG102");
 
 
-        boolean hasSleeper = passengerBogies.contains("Sleeper");
-        System.out.println("\nChecking if 'Sleeper' exists:");
-        System.out.println("Contains Sleeper? : " + hasSleeper);
+        System.out.println("\nBogie IDs After Insertion:");
+        System.out.println(bogies);
+
+        System.out.println("\nNote: \nDuplicates were automatically ignored by HashSet.");
 
 
-        System.out.println("\nFinal Train Passenger Consist: " + passengerBogies);
-        System.out.println("UC2 operations completed successfully ...");
+        System.out.println("\nUC3 uniqueness validation completed successfully...");
     }
 }
